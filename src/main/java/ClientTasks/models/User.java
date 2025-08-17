12955cd46db528119847a52f714478f634ae4a27
Gabriel <gabrielgvo16@gmail.com>
2025-08-17 16:53:31 -0300

    Create user model

---
+++ src/main/java/ClientTasks/models/User.java
@@ -0,0 +1,69 @@
+package ClientTasks.models;
+
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.scheduling.config.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+
+public final class User {
+    public static final String TABLE_NAME = "users";
+
+    public interface CreateUser{}
+    public interface UpdateUser{}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", unique = true)
+    private Long id;
+
+    @Column(name = "USERNAME", unique = true, length =  100, nullable = false)
+    @NotNull
+    private String username;
+
+    @Column(name = "PASSWORD", length = 60, nullable = false)
+    @NotNull
+
+    private String password;
+
+    //private ArrayList<Task> tasks = new ArrayList<Task>();
+
+    public User() {
+    }
+
+    public User(long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username)  {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User user = (User) obj;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password);
+    }
+}
